e0c7ec855e5fb3d8a53030e8e0aba887ec544bad
LuFelix <luciano@tiweb.app.br>
2025-07-16 10:50:02 -0300

    feat: Implements initial Landing Page structure and styles (Hero, Sections and Footer)

--- frontend-angular/src/app/app.component.ts
+++ frontend-angular/src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component'; // Importe o HeaderComponent
+import { FooterComponent } from './components/footer/footer.component'; // Importe o FooterComponent
+import { HomeComponent } from './pages/home/home.component'; // Importe o HomeComponent
+
+// Importe MatSidenavModule
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'frontend-angular';
